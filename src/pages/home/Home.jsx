38360be3a73414b572d9ce1cd971776b1d3855a5
Valeriy Nehovora <nehovora.val@gnail.com>
2022-04-06 22:50:50 -0400

    implement RecipeList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+
+// components
+import RecipeList from "../components/RecipeList";
 
 // style
 import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
