6716c870e6d46fd16cec951e2ed8ddad38cbbe5c
Valeriy Nehovora <nehovora.val@gnail.com>
2022-04-07 10:38:20 -0400

    add route fot each recipe

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 // style
 import "./Recipe.css";
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push("/");
+      }, 2000);
+    }
+  }, [error, history]);
+
+  return (
+    <div className="recipe">
+      {isPending && <div className="loading">Loading...</div>}
+      {error && <div className="error">{error}</div>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+
+          <p className="method">Cooking method - {recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
