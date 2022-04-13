ed952c7ba3dfd89db3aa0b80f237b28af40026d3
Valeriy Nehovora <nehovora.val@gnail.com>
2022-04-12 20:34:16 -0400

    add mode button

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTheme } from "../hooks/useTheme";
+import modeIcon from "../assets/mode-switch-icon.svg";
 
 // style
 import "./ThemeSelector.css";
@@ -7,10 +8,22 @@ import "./ThemeSelector.css";
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === "dark" ? "light" : "dark");
+  };
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          onClick={toggleMode}
+          alt="dark/light toggle icon"
+          style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map(color => (
           <div
